24b53f0e3cb38562bb9f0324502c324e8d4288f3
Ankit Khedar <57590215+ankitkhedar12@users.noreply.github.com>
2023-04-13 21:58:29 +0530

    saf

--- app/Movie.tsx
+++ app/Movie.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default  function Movie({title, id, poster_path, release_date}:any){
     const imagePath = "https://image.tmdb.org/t/p/original"
     return (
-        <div className="hover:bg-slate-700 rounded-md  px-9 py-2" >
-            <h1 className=" antialiased bg-slate-800 hover:bg-slate-700 rounded-md inline-block my-2 py-2 px-4" >{title}</h1>
-            <h2 className=" antialiased bg-slate-900 rounded-md inline-block my-2 py-2 px-4" >{release_date}</h2>
+        <div className="hover:bg-orange-700 rounded-md  mx-2 px-2 py-2 " >
             <Link href={`/${id}`}>
                 <Image className="rounded" src={imagePath + poster_path} alt={title} width={300} height={300} />
             </Link>
+            <h1 className=" antialiased bg-cyan-600 hover:bg-slate-700 rounded-md inline-block my-2 py-2 px-4" >{title}</h1>
+            <h2 className=" antialiased rounded-md inline-block my-2 py-2 px-4" >{release_date}</h2>
         </div>
     )
 }
